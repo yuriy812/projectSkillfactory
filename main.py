f8e0a149d9883bc554e608f592e147893856c998
yuriy <sitecadilac@mail.ru>
2024-07-20 23:57:31 +0300

    mutipl

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+multipl = 1
 for i in range(1, 11):
-    for j in range(1, 11):
-        print(f'{i} * {j} = {i * j}')
-print()
+    if i % 2 == 0:
+        continue
+    multipl += i
+print(multipl)
