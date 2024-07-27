a4b53a098478c773ae56900c593981006219a8a6
yuriy <sitecadilac@mail.ru>
2024-07-27 13:45:37 +0300

    Структура проекта1

--- practicumSkillfactor.py
+++ practicumSkillfactor.py
@@ -12,7 +12,7 @@ students_marks = {}
 for student in students:
     students_marks[student] = {}
     for class_ in classes:
-        marks = [random.randint(1,5) for i in range(3)]  # generate a list of 3 random marks
+        marks = [random.randint(1, 5) for i in range(3)]  # generate a list of 3 random marks
         students_marks[student][class_] = marks
 # Print the dictionary of student marks
 for student in students:
