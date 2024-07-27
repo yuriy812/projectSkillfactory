6f33ace708e4e772d1c6cffb79e205e32851aebc
yuriy <sitecadilac@mail.ru>
2024-07-27 14:36:41 +0300

    Структура проекта4

--- practicumSkillfactor.py
+++ practicumSkillfactor.py
@@ -12,9 +12,60 @@ students_marks = {}
 for student in students:
     students_marks[student] = {}
     for class_ in classes:
-        marks = [random.randint(1,5) for i in range(3)]  # generate a list of 3 random marks
+        marks = [random.randint(1, 5) for i in range(3)]  # generate a list of 3 random marks
         students_marks[student][class_] = marks
 # Print the dictionary of student marks
 for student in students:
     print(f'''{student}
     {students_marks[student]}''')
+print()
+print('''Список команд:
+        1. Добавить оценку ученика по предмету
+        2. Вывести средний балл по всем предметам по каждому ученику
+        3. Вывести все оценки по всем ученикам
+        4. Выход из программы''')
+while True:
+    command = int(input('Введите команду: '))
+    if command == 1:
+        print('1. Добавить оценку ученика по предмету')
+        # считываем имя ученика
+        student = input('Введите имя ученика: ')
+        # считываем название предмета
+        class_ = input('Введите предмет: ')
+        # считываем оценку
+        mark = int(input('Введите оценку: '))
+        # если данные введены верно
+        if student in students_marks.keys() and class_ in students_marks[student].keys():
+            # добавляем новую оценку для ученика по предмету
+            students_marks[student][class_].append(mark)
+            print(f'Для {student} по предмету {class_} добавлена оценка {mark}')
+        # неверно введены название предмета или имя ученика
+        else:
+            print('ОШИБКА: неверное имя ученика или название предмета')
+    elif command == 2:
+        print('2. Вывести средний балл по всем предметам по каждому ученику')
+        # цикл по ученикам
+        for student in students:
+            print(student)
+            # цикл по предметам
+            for class_ in classes:
+                # находим сумму оценок по предмету
+                marks_sum = sum(students_marks[student][class_])
+                # находим количество оценок по предмету
+                marks_count = len(students_marks[student][class_])
+                # выводим средний балл по предмету
+                print(f'{class_} - {marks_sum // marks_count}')
+            print()
+    elif command == 3:
+        print('3. Вывести все оценки по всем ученикам')
+        # выводим словарь с оценками:
+        # цикл по ученикам
+        for student in students:
+            print(student)
+            # цикл по предметам
+            for class_ in classes:
+                print(f'\t{class_} - {students_marks[student][class_]}')
+            print()
+    elif command == 4:
+        print('4. Выход из программы')
+        break
