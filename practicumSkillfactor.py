04f3762495eb4cb9bb6ba12741128a990cfe5386
yuriy <sitecadilac@mail.ru>
2024-07-27 13:33:19 +0300

    стрктура проекта

--- practicumSkillfactor.py
+++ practicumSkillfactor.py
@@ -1,6 +1,20 @@
-multipl = 1
-for i in range(1, 10):
-    if i % 2 == 0:
-        continue
-    multipl *= i
-print(multipl)
+import random
+
+# List of students
+students = ['Аполлон', 'Ярослав', 'Александра', 'Дарья', 'Ангелина']
+# Sort the list of students
+students.sort()
+# List of subjects
+classes = ['Математика', 'Русский язык', 'Информатика']
+# Empty dictionary to store student marks
+students_marks = {}
+# Generate random marks for each student and subject combination
+for student in students:
+    students_marks[student] = {}
+    for class_ in classes:
+        marks = [random.randint(1,5) for i in range(3)]  # generate a list of 3 random marks
+        students_marks[student][class_] = marks
+# Print the dictionary of student marks
+for student in students:
+    print(f'''{student}
+    {students_marks[student]}''')
